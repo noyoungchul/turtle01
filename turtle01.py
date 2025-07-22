d275513e7f6940e7427497f186b80214cecbfb68
noyoungchul <shdudcjf107@gmail.com>
2025-07-22 10:35:44 +0900

    장애물 이동
    장애물이동

--- turtle01.py
+++ turtle01.py
@@ -5,4 +5,15 @@ s = turtle.getscreen()
 
 # 거북이 변수 지정
 t = turtle.Turtle()
+t.shape("turtle")
+for i in range(4):
+    t.fd(100)
+    t.rt(90)
+    
+    
+t.penup()    
+t.goto(-350,-350)
+t.goto(350,350)
+
+
 
